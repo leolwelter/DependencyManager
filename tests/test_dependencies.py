6abd4fc62f36b83c0f95ea0e0f20da098dc2969b
leolwelter <leolwelter@gmail.com>
2021-02-08 14:00:06 -0500

    Add beginning unit tests

--- tests/test_dependencies.py
+++ tests/test_dependencies.py
@@ -1,4 +1,6 @@
 import unittest
+import io
+from contextlib import redirect_stdout
 
 from src.main import Dependencies
 
@@ -9,6 +11,17 @@ class TestDependencies(unittest.TestCase):
 
     def test_smoke(self):
         self.assertIsNotNone(self.deps)
+
+    def test_sample(self):
+        with open('../sample-out.txt') as inf:
+            expected = inf.read().strip()
+        with open('../sample.txt') as inf:
+            commands = [x.strip().split() for x in inf.readlines()]
+
+        buffer = io.StringIO()
+        with redirect_stdout(buffer):
+            self.deps.run_commands(commands)
+        self.assertEqual(expected.strip(), buffer.getvalue().strip())
 
 if __name__ == '__main__':
     unittest.main()
